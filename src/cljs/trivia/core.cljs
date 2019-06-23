8e5e4ac06b24a08c37281ab5b8269ef41e336caf
Justin Duncan <justin@baal-software.com>
2019-06-23 09:48:11 -0600

    Adds initial function that renders to browser agent

--- src/cljs/trivia/core.cljs
+++ src/cljs/trivia/core.cljs
@@ -1,1 +1,9 @@
-(ns trivia.core)
+(ns trivia.core
+  (:require [reagent.core :as reagent]))
+
+(defn mount-root []
+  (reagent/render [:h1 "Hello from Clojure Script!"]
+                  (.getElementById js/document "app")))
+
+(defn ^:export init []
+  (mount-root))
